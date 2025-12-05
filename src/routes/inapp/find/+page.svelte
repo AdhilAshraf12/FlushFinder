ee2c4f6c534e040942880c35d2f9fddce54980a2
Fatima Asif <fatima.asif@ucalgary.ca>
2025-12-05 02:23:18 -0700

    find page styled

--- src/routes/inapp/find/+page.svelte
+++ src/routes/inapp/find/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { userInfo } from '$lib/userInfoStore';
+	import { goto } from '$app/navigation';
 	import map from '$lib/assets/mapsimage.png';
 
 	$effect(() => {
@@ -7,6 +8,7 @@
 			location.href = '/';
 		}
 	});
+
 </script>
 
 <form class="searchbar-full"
@@ -20,7 +22,7 @@
 			><img src="https://www.svgrepo.com/show/524617/hamburger-menu.svg" alt="Hamburger" /></button
 		>
 		<input type="text" placeholder="Search for bathrooms" class= "search-bar"/>
-		<button type="submit">
+		<button type="submit" class="searchicon">
 			<img src="https://www.svgrepo.com/show/532552/search-alt-2.svg" alt="Search Icon" />
 		</button>
 	</div>
@@ -33,12 +35,96 @@
 	</span>
 	
 </div>
-
-<div class="nearby locations">
-		
+<div class="nearbylocations">
+	<div class="buttonrow">
+		<button class="locationbut">
+			Deville Coffee Washroom
+			<small class="small">100 m&nbsp;&nbsp;&nbsp;&nbsp;2 min  🚻    ♿</small>
+		</button>
+		<div class="gocol">
+			<div>⭐️⭐️⭐️⭐️⭐️</div>
+			<button class="FSbutton" onclick={() => goto('/inapp/deville')}>
+				GO
+			</button>
+		</div>
+	</div>		
+	<div class="buttonrow">
+		<button class="locationbut">
+			BVC Main floor Washroom
+			<small class="small">300 m&nbsp;&nbsp;&nbsp;&nbsp;6 min&nbsp; ⚧    ♿</small>
+		</button>
+		<div class="gocol">
+			<div>⭐️⭐️⭐️⭐️</div>
+			<button class="FSbutton">
+				GO
+			</button>
+		</div>
+	</div>	
+	<div class="buttonrow">
+		<button class="locationbut">
+			Superstore Washroom
+			<small class="small">350 m&nbsp;&nbsp;&nbsp;&nbsp;8 min  🚻    ♿</small>
+		</button>
+		<div class="gocol">
+			<div>⭐️⭐️⭐️</div>
+			<button class="FSbutton">
+				GO
+			</button>
+		</div>
+	</div>	
 </div>
 
 <style>
+	.gocol{
+		display:flex;
+		flex-direction: column;
+		align-items: center;
+		width: 90px;
+	}
+
+	.small{
+		color: #757575;
+	}
+
+	.buttonrow{
+		display:flex;
+		flex-direction: row;
+		padding-right: 5px;
+	}
+	.locationbut{
+		background-color: white;
+		/* border: 1px solid purple; */
+		margin: 10px;
+		color: 1E1E1E;
+		font-size: 18px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 0 10px;
+		height: auto;
+		width: 235px;
+		text-align: left;
+		align-items: flex-start;
+	}
+
+	.nearbylocations{
+		border: 1px solid #D9D9D9;
+		box-shadow: 0px 4px 4px 0px rgb(0, 0, 0, 0.25);
+		border-radius: 8px;
+		height: 220px;
+		width: 360px;
+		display: flex;
+		flex-direction: column;
+		margin-left: auto;
+  		margin-right: auto;
+		justify-content: space-evenly;
+	}
+
+	.searchicon{
+		margin-left: auto;
+		border-radius: 15px;
+	}
+
 	img {
 		width: 30px;
 		aspect-ratio: 1;
@@ -52,17 +138,18 @@
 		border-radius: 100px;
 	}
 
-	.filterbuttons{
+	.filterbuttons span{
 		display:flex;
-		justify-content: center;
+		justify-content: space-between;
+		
 	}
 
 	.div-search {
-		border: 2px solid red;
+		/* border: 2px solid red; */
 		display: flex;
 		background-color: #ECE6F0;
 		align-items: center;
-		justify-content: center;
+		justify-content: left;
 		height: 56px;
 		width: 360px;
 		border-radius: 40px;
@@ -82,7 +169,7 @@
 		border: none;
 	}
 	.searchbar-full{
-		border: 2px solid green;
+		/* border: 2px solid green; */
 		display: flex;
 		justify-content: top;
 		flex-direction: column;
